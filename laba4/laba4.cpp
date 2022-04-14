ba48828344d713cb380638aef10f8699bcdb9b6d
Мария Андреева <AndreevaMM@stud.kai.ru>
2022-04-14 14:44:11 +0300

    я смогла создать файл обожемой

--- laba4/laba4.cpp
+++ laba4/laba4.cpp
@@ -1,64 +1,25 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <cmath>
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
-void counter(string s[]) {
-    
-}
-
-void fail(string path) {
-    cout << "Хорошо, читаю файл." << endl;
-    if (path == "") {
-        path = "4laba.txt";
-    }
-    ifstream fin(path);
-    while (!fin.is_open()) {
-        cout << "Файл невозможно открыть. Повторите имя файла." << endl;
-        cin >> path;
-        cout << "Хорошо, читаю файл." << endl;
-        ifstream fin(path);
-    }
-    int n;
-    fin >> n;
-    string* s;
-    fin >> s[n];
-    counter(s);
-}
-
-void cons() {
-    cout << "Хорошо, вводите данные. Для начала введите число n." << endl;
-    int n;
-    cin >> n;
-    cout << "Теперь введите символы s1...sn." << endl;
-    string* s;
-    cin >> s[n];
-    counter(s);
-    cout << "Хотите повторить ввод исходных данных ? Да — 1, Нет — 0." << endl;
-    int x;
-    cin >> x;
-    if (x == 1) {
-        cons();
-    }
-    else {
-        cout << "Хорошо, завершаю работу." << endl;
-    }
-}
-int main()
-{
+int main() {
     setlocale(LC_ALL, "rus");
     cout << "Вы хотите ввести данные из файла или из консоли?" << endl;
     cout << "0 - из файла, 1 - из консоли." << endl;
     int a;
-    cin >> a;
+    (cin >> a).get();
     if (a == 1) {
-        cons();
+        return 0;
     }
     else {
-        cout << "Введите путь до файла." << endl;
-        string path;
-        cin >> path;
-        fail(path);
+        cout << "Введите полное имя файла. Напишите нет, если не хотите вводить полное имя." << endl;
+        string filename;
+        getline(cin, filename);
+        ofstream fout(filename.c_str());
+        fout << "i'm dead";
+        fout.close();
     }
 }
